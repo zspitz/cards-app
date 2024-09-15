b8b20cfaca98be8ebcd4976fbcec108c1640b04d
Zev Spitz <shivisi@hotmail.com>
2024-09-15 14:37:05 +0300

    Position UI for card actions

--- src/components/cards/MasonryCard.tsx
+++ src/components/cards/MasonryCard.tsx
@@ -1,6 +1,7 @@
-import { Card, Image, Paper, Stack, Text } from '@mantine/core'
+import { ActionIcon, Card, Group, Image, Paper, Stack, Text } from '@mantine/core'
 import { CardResponse } from '../../types'
 import classes from './MasonryCard.module.css'
+import { TbHeart, TbHeartFilled, TbTrash, TbPencil } from 'react-icons/tb'
 
 type Props = {
     index: number,
@@ -9,12 +10,31 @@ type Props = {
 }
 
 const MasonryCard = ({ data: card }: Props) => (
-    <Card radius="md" key={card._id} m="xs" p="0" className={classes.card}>
+    <Card radius="md" key={card._id} m="xs" p="0" className={classes.card} mih={150}>
         <Image src={card.image.url} alt={card.image.alt} className={classes.image} />
         <Stack pos="absolute" gap={0} justify="flex-end" h="100%" w="100%">
             <Paper w="100%" radius={0} opacity={.75} p={5}>
-                <Text size="lg">{card.title}</Text>
-                <Text size="xs">{card.subtitle}</Text>
+                <Group gap={0}>
+                    <Stack gap={0} flex="1">
+                        <Text size="lg">{card.title}</Text>
+                        <Text size="xs">{card.subtitle}</Text>
+                    </Stack>
+                    <Group gap={0}>
+                        <ActionIcon variant="transparent">
+                            {
+                                Math.random() > .5 ?
+                                    <TbHeart color="#e33bc4" /> :
+                                    <TbHeartFilled color="#e33bc4" />
+                            }
+                        </ActionIcon>
+                        <ActionIcon variant="transparent">
+                            <TbPencil />
+                        </ActionIcon>
+                        <ActionIcon variant="transparent">
+                            <TbTrash />
+                        </ActionIcon>
+                    </Group>
+                </Group>
             </Paper>
         </Stack>
     </Card>
